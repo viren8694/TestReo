2c9089f746749263d2aec237417a7760ceb3e16a
Viren_Github <viren8694@gmail.com>
2018-02-20 10:38:06 -0600

    First Github Commit
    First Github Commit

---
+++ GithubPractise/GithubPractise/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GithubPractise
+//
+//  Created by Viren Patel on 2/20/18.
+//  Copyright © 2018 Viren Patel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
